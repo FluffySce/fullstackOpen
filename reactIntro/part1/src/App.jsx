d5a1c7eb38fd2418f5d867ed0071e5634049de26
FluffySce <73637814+FluffySce@users.noreply.github.com.>
2025-12-15 22:38:46 +0530

    refactor: extract display and button components for better readability

--- reactIntro/part1/src/App.jsx
+++ reactIntro/part1/src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from "react";
 
+const Display = ({ counter }) => {
+  return <div>{counter}</div>;
+};
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
 const App = () => {
   const [counter, setCounter] = useState(0);
   const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
   const setToZero = () => setCounter(0);
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>plus</button>
-      <button onClick={setToZero}>reset</button>
+      <Display counter={counter}></Display>
+      <Button onClick={increaseByOne} text={"plus"}></Button>
+      <Button onClick={decreaseByOne} text={"decrease"}></Button>
+      <Button onClick={setToZero} text={"reset"}></Button>
     </div>
   );
 };
