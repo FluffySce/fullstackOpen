e173b1d44c114c56e8fb2e2d63ff6fdc9b0139ec
FluffySce <73637814+FluffySce@users.noreply.github.com.>
2025-12-17 22:38:50 +0530

    learned to use and implement complex stats

--- reactIntro/part1/src/App.jsx
+++ reactIntro/part1/src/App.jsx
@@ -1,26 +1,59 @@
 import { useState } from "react";
 
-const Display = ({ counter }) => {
-  return <div>{counter}</div>;
-};
-
 const Button = ({ onClick, text }) => {
   return <button onClick={onClick}>{text}</button>;
 };
 
-const App = () => {
-  const [counter, setCounter] = useState(0);
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
-
+const History = ({ allClicks }) => {
+  if (allClicks.length === 0) {
+    return (
+      <div>
+        {" "}
+        <p>the app is used by pressing a button</p>
+      </div>
+    );
+  }
   return (
     <div>
-      <Display counter={counter}></Display>
-      <Button onClick={increaseByOne} text={"plus"}></Button>
-      <Button onClick={decreaseByOne} text={"decrease"}></Button>
-      <Button onClick={setToZero} text={"reset"}></Button>
+      <p>button press history: {allClicks.join(" ")}</p>
     </div>
   );
 };
+
+const App = () => {
+  const [clicks, setClicks] = useState({ left: 0, right: 0 });
+  const [allClicks, setAllClicks] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const handleLeftClick = () => {
+    setClicks({ ...clicks, left: clicks.left + 1 });
+    setAllClicks(allClicks.concat("L"));
+    setTotal(total + 1);
+  };
+  const handleRightClick = () => {
+    setClicks({ ...clicks, right: clicks.right + 1 });
+    setAllClicks(allClicks.concat("R"));
+    setTotal(total + 1);
+  };
+  const hello = (who) => {
+    () => {
+      console.log("Hello", who);
+    };
+  };
+
+  return (
+    <div>
+      {clicks.left}
+      <Button onClick={handleLeftClick} text={"Left+1"}></Button>
+      <Button onClick={handleRightClick} text={"right+1"}></Button>
+      {clicks.right}
+      <History allClicks={allClicks}></History>
+      <p>Total - {total}</p>
+      <Button onClick={hello("react")} text={"testing something"}></Button>
+      <Button onClick={hello("world")} text={"testing something"}></Button>
+      <Button onClick={hello("idk")} text={"testing something"}></Button>
+    </div>
+  );
+};
+
 export default App;
