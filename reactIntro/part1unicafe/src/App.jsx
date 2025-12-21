572958b6b8894434ae8a4a91a18b8f1a1794b606
FluffySce <73637814+FluffySce@users.noreply.github.com.>
2025-12-21 22:44:08 +0530

    implement and reused multiple components : understood props and passing better

--- reactIntro/part1unicafe/src/App.jsx
+++ reactIntro/part1unicafe/src/App.jsx
@@ -4,13 +4,39 @@ const Button = ({ onClick, text }) => {
   return <button onClick={onClick}>{text}</button>;
 };
 
-const Statistics = ({ ratings }) => {
+const StatisticLine = ({ text, value }) => {
   return (
-    <div>
-      <p>good: {ratings.good}</p>
-      <p>bad: {ratings.bad}</p>
-      <p>neutral: {ratings.neutral}</p>
-    </div>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ ratings }) => {
+  const total = ratings.good + ratings.bad + ratings.neutral;
+
+  if (total === 0) {
+    return <p>No Feedback Given</p>;
+  }
+
+  const average = (ratings.good - ratings.bad) / total;
+  const positive = (ratings.good / total) * 100;
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text={"Good"} value={ratings.good}></StatisticLine>
+        <StatisticLine text={"Bad"} value={ratings.bad}></StatisticLine>
+        <StatisticLine text={"Neutral"} value={ratings.neutral}></StatisticLine>
+        <StatisticLine text={"Total"} value={total}></StatisticLine>
+        <StatisticLine text={"Average"} value={average}></StatisticLine>
+        <StatisticLine
+          text={"Positive"}
+          value={`${positive} %`}
+        ></StatisticLine>
+      </tbody>
+    </table>
   );
 };
 
@@ -18,15 +44,15 @@ const App = () => {
   const [ratings, setRatings] = useState({ good: 0, bad: 0, neutral: 0 });
   const handleGood = () => {
     setRatings({ ...ratings, good: ratings.good + 1 });
-    console.log("good", ratings.good + 1);
   };
   const handleBad = () => {
     setRatings({ ...ratings, bad: ratings.bad + 1 });
-    console.log("bad", ratings.bad + 1);
   };
   const handleNeutral = () => {
     setRatings({ ...ratings, neutral: ratings.neutral + 1 });
-    console.log("neutral", ratings.neutral + 1);
+  };
+  const handleReset = () => {
+    setRatings({ good: 0, bad: 0, neutral: 0 });
   };
 
   return (
@@ -35,6 +61,7 @@ const App = () => {
       <Button onClick={handleGood} text={"good"}></Button>
       <Button onClick={handleBad} text={"bad"}></Button>
       <Button onClick={handleNeutral} text={"neutral"}></Button>
+      <Button onClick={handleReset} text={"reset"}></Button>
       <h1>Stastics</h1>
       <Statistics ratings={ratings}></Statistics>
     </div>
